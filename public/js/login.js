8c480d5b46ef7b40ab231e92c70d350836d0a492
AbhijitDey02dante <abhijitdey512@gmail.com>
2022-05-07 19:09:49 +0530

    LoginScreen frontend

---
+++ public/js/login.js
@@ -0,0 +1,19 @@
+const email=document.querySelector('#email');
+const password=document.querySelector('#password');
+
+const form=document.querySelector('.signupForm');
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const obj={
+        email:email.value,
+        password:password.value
+    }
+    console.log(obj);
+
+    axios.post("http://localhost:3000/login",obj)
+    .then((res)=>{
+        console.log(res)
+    })
+    .catch(error=>console.log(error));
+})
